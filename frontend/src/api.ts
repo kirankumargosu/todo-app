5419a9f57b19117f7b44ce67d6352e915d79749c
kirankumargosu <kirankumar.gosu@gmail.com>
2025-12-15 19:52:58 +0000

    added env variable for backend end point

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { Task } from "./types";
 
-const API_URL = "http://localhost:8000";
+// const API_URL = "http://localhost:8000";
+const API_URL = process.env.REACT_APP_API_URL!;
 
 export const getTasks = () => axios.get<Task[]>(`${API_URL}/tasks`);
 
