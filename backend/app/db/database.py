9e081bec6775d9811beee7734a4209be30ced2c9
kirankumargosu <kirankumar.gosu@gmail.com>
2025-12-22 09:29:08 +0000

    moved db to persistent storage

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 from logging import getLogger
 logger = getLogger(__name__)
-DATABASE_URL = "sqlite:///./todo.db"
+
+# DATABASE_URL = "sqlite:///./todo.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL not set")
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
